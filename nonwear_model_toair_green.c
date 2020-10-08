85e1fdcbb39516d75328c2badca9c7fc178b6b3f
Ziyi Liu <lc_lc2000@vip.163.com>
2020-10-08 14:45:28 +0800

    435-dev: 临时保存.

--- nonwear_model_toair_green.c
+++ nonwear_model_toair_green.c
@@ -3,12 +3,16 @@
 
 #include "nonwear_model.h"
 
+int8_t replace = 1;
+
 float PredictGreenAir(union NonwearEntry* data) {
   float sum = 0.0f;
   // unsigned int tmp;
-  if (!(data[0].missing != -1) || (data[0].fvalue < 0.67209923f)) {
-    if (!(data[1].missing != -1) || (data[1].fvalue < 1231.5667f)) {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -4259.7231f)) {
+  if (!(data[0].missing != -replace) || (data[0].fvalue < 0.67209923f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < 1231.5667f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -4259.7231f)) {
         sum += 0.59916496f;
       } else {
         sum += 0.46233502f;
@@ -17,19 +21,22 @@ float PredictGreenAir(union NonwearEntry* data) {
       sum += -0.57837844f;
     }
   } else {
-    if (!(data[1].missing != -1) || (data[1].fvalue < -4249.7539f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < -4249.7539f)) {
       sum += 0.59220779f;
     } else {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -2067.0735f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -2067.0735f)) {
         sum += -0.54250222f;
       } else {
         sum += -0.59670961f;
       }
     }
   }
-  if (!(data[0].missing != -1) || (data[0].fvalue < 0.67209923f)) {
-    if (!(data[1].missing != -1) || (data[1].fvalue < 1231.5667f)) {
-      if (!(data[0].missing != -1) || (data[0].fvalue < 0.47561854f)) {
+  if (!(data[0].missing != -replace) || (data[0].fvalue < 0.67209923f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < 1231.5667f)) {
+      if (!(data[0].missing != -replace) || (data[0].fvalue < 0.47561854f)) {
         sum += 0.45002547f;
       } else {
         sum += 0.23963501f;
@@ -38,19 +45,23 @@ float PredictGreenAir(union NonwearEntry* data) {
       sum += -0.44997427f;
     }
   } else {
-    if (!(data[1].missing != -1) || (data[1].fvalue < -4249.7539f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < -4249.7539f)) {
       sum += 0.45934191f;
     } else {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -2402.9102f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -2402.9102f)) {
         sum += -0.40951529f;
       } else {
         sum += -0.46085042f;
       }
     }
   }
-  if (!(data[0].missing != -1) || (data[0].fvalue < 0.65745008f)) {
-    if (!(data[1].missing != -1) || (data[1].fvalue < 1231.5667f)) {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -4259.7231f)) {
+  if (!(data[0].missing != -replace) || (data[0].fvalue < 0.65745008f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < 1231.5667f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -4259.7231f)) {
         sum += 0.40637055f;
       } else {
         sum += 0.26543957f;
@@ -59,19 +70,23 @@ float PredictGreenAir(union NonwearEntry* data) {
       sum += -0.38692912f;
     }
   } else {
-    if (!(data[1].missing != -1) || (data[1].fvalue < -4249.7539f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < -4249.7539f)) {
       sum += 0.40038767f;
     } else {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -2067.0735f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -2067.0735f)) {
         sum += -0.34640157f;
       } else {
         sum += -0.40067038f;
       }
     }
   }
-  if (!(data[0].missing != -1) || (data[0].fvalue < 0.61521512f)) {
-    if (!(data[1].missing != -1) || (data[1].fvalue < 1231.5667f)) {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -4260.7505f)) {
+  if (!(data[0].missing != -replace) || (data[0].fvalue < 0.61521512f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < 1231.5667f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -4260.7505f)) {
         sum += 0.3702296f;
       } else {
         sum += 0.23560536f;
@@ -80,19 +95,23 @@ float PredictGreenAir(union NonwearEntry* data) {
       sum += -0.34440771f;
     }
   } else {
-    if (!(data[1].missing != -1) || (data[1].fvalue < -4249.7539f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < -4249.7539f)) {
       sum += 0.36747366f;
     } else {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -2067.0735f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -2067.0735f)) {
         sum += -0.30749041f;
       } else {
         sum += -0.36446184f;
       }
     }
   }
-  if (!(data[0].missing != -1) || (data[0].fvalue < 0.53856802f)) {
-    if (!(data[1].missing != -1) || (data[1].fvalue < 1575.5f)) {
-      if (!(data[1].missing != -1) || (data[1].fvalue < -4258.0234f)) {
+  if (!(data[0].missing != -replace) || (data[0].fvalue < 0.53856802f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < 1575.5f)) {
+      if (!(data[replace].missing != -replace) ||
+          (data[replace].fvalue < -4258.0234f)) {
         sum += 0.34766379f;
       } else {
         sum += 0.22812618f;
@@ -101,10 +120,11 @@ float PredictGreenAir(union NonwearEntry* data) {
       sum += -0.30775887f;
     }
   } else {
-    if (!(data[1].missing != -1) || (data[1].fvalue < -4249.7539f)) {
+    if (!(data[replace].missing != -replace) ||
+        (data[replace].fvalue < -4249.7539f)) {
       sum += 0.34680972f;
     } else {
-      if (!(data[0].missing != -1) || (data[0].fvalue < 0.62959903f)) {
+      if (!(data[0].missing != -replace) || (data[0].fvalue < 0.62959903f)) {
         sum += -0.59450001f;
       } else {
         sum += -0.3210547f;
--- nonwear_process.c
+++ nonwear_process.c
@@ -4,8 +4,44 @@
 #define NWC_REQUEST_ACC (1)
 
 #define NWC_TIMER_TO_AIR (0)
+#define NWC_NONWEAR_CHECK_DELAY (3) /*<< 3min. */
 
 static uint16_t k_timer_to_air_delay;
+
+/* 当前佩戴状态 - 当前测量模式 - 对空状态 - 对空计时器 - 非空 - 静止计时器 -
+ * 运动计时器
+ * - 活体检测计时器 */
+/* 1. 连续测量模式: NWC_WEAR + 对空 + 对空计时0s + 非空计时0s + xx静止计时 +
+ * xx运动计时 + xx活体计时 -> 首次对空切红外 -> NWC_NONWEAR.
+ * 2. 连续测量模式: NWC_DETERM_WEAR + 对空 + 对空计时0s + 非空计时0s +
+ * xx静止计时 + xx运动计时
+ * + xx活体计时 -> 首次对空切红外 -> NWC_NONWEAR.
+ * + 任何活体计时 -> 首次对空切红外 -> NWC_NONWEAR.
+ * 3. 连续测量模式: NWC_NONWEAR + 对空 + 对空计时10s + 任何静止计时 +
+ * 任何运动计时
+ * + 任何活体计时 -> 对空延时关所有灯 -> NWC_DETERM_NONWEAR.
+ * 4. 连续测量模式: NWC_NONWEAR + 非对空 + 非对空计时0s + 任何静止计时 +
+ * 任何运动计时
+ * + 任何活体计时
+ */
+
+struct decision_node decision_nodes = [
+
+  {}, /*<< NWC_ONWEAR -> 1. 连续测量模式 - 对空 - 对空计时0 - 任何静止时间 -
+         任何运动时间 - 任何活体时间 -> (首次对空)切红外 */
+  {}, /*<< NWC_ONWEAR -> 1. 连续测量模式 - 对空 - 对空计时10s - 任何静止时间 -
+         任何运动时间 - 任何活体时间 -> (对空延时)关所有灯 */
+  {}, /*<< NWC_DETERM_ONWEAR -> 2. - 任何测量模式 - 对空- 对空计时0 -
+         任何静止时间 - 任何运动时间 - 任何活体时间 -> (首次对空)切红外 */
+  {},
+];
+
+struct decision_node {
+  uint8_t
+      status[NUM_SUPPORT]; /*<< 支持的可切灯状态: 当前佩戴状态, 模式，运动...*/
+  switch_led_t sl;           /*<< 需要切换到的灯. */
+  switch_nonwear_state_t sn; /*<< 需要切换到的佩戴状态. */
+};
 
 /* TODO: 参数配置. */
 
@@ -16,6 +52,8 @@ static uint16_t k_timer_to_air_delay;
   }
 }
 
+/* 切灯处理&状态转换处理. */
+
 /* TODO: 运动状态判断. */
 static void _ACCCheck(nwc_bioSignal_t* sig) {
   if ("运动") { /* 运动判断需要封装连续运动次数判断和抬腕亮屏. */
@@ -24,8 +62,6 @@ static void _ACCCheck(nwc_bioSignal_t* sig) {
     _TimerStart(MOTION);
     if (k_nonwear_state == NWC_DETERM_NONWEAR) {
       /*<< NWC_DETERM_NONWEAR下灯已经关闭，必须置为NWC_NONWEAR. */
-      // k_nonwear_state = NWC_NONWEAR;
-      // k_switch_led = true;
       _Request(NWC_NONWEAR);
     } else if (k_nonwear_state == NWC_DETERM_WEAR) {
       /*<< 非连续心率且NWC_DETERM_WEAR时，. */
@@ -43,45 +79,42 @@ static void _ACCCheck(nwc_bioSignal_t* sig) {
 
 /* TODO: 对空判断. */
 static void _PPGCheck(nwc_bioSignal_t* sig) {
-  static uint8_t nonwear_check_init = 0u;
-
   /* 对空判断阶段. */
   NonwearToAirCheck(sig); /*<< 改写自ToAirCheck. */
   if (k_to_air) {
-    if (k_nonwear_state != NWC_NONWEAR) {
-      _Request(NWC_REQUEST_PPG);                        /*<< NWC_NONWEAR. */
-      _TimerStart(eTIMER_TO_AIR, k_timer_to_air_delay); /*<< 开始对空计时. */
-    } else {
-      if (_TimerGetStatus(eTIMER_TO_AIR)) {
-        _Request(NWC_REQUEST_PPG); /*<< NWC_DETERM_NONWEAR. */
-        _TimerEnd(eTIMER_TO_AIR);  /*<< 结束对空计时. */
-      }
+    if (timer_to_air->u16DelayMs == 0) {
+      timer_to_air = _TimerStart(eTIMER_TO_AIR); /*<< 开始对空计时. */
+      _TimerEnd(timer_not_to_air);
     }
+    _Request(NWC_REQUEST_PPG);
     return;
   } else {
-    if (k_nonwear_state == NWC_NONWEAR) {
-      _Request(NWC_REQUEST_PPG); /*<< NWC_WEAR. */
+    if (timer_not_to_air->u16DelayMs == 0) {
+      timer_not_to_air = _TimerStart(eTIMER_NOT_TO_AIR);
+      _TimerEnd(timer_to_air);
       return;
     }
+    _Request(NWC_REQUEST_PPG); /*<< NWC_WEAR. */
   }
 
   /* 活体检测. */
   if (k_nonwear_state == NWC_WEAR) {
-    if (!) {
+    /* 模式切换时做各种初始化. */
+    if (timer_nonwear_check_timeout->u16DelayMs == 0) {
       NonWearCheck(NULL, true);
-      _TimerStart("活体", 0);
+      timer_nonwear_check_timeout = _TimerStart(eTIMER_CHECK_TIMEOUT);
     }
 
     uint8_t nonwear_state = NonWearCheck(sig, false);
-    if (nonwear_state) {
+    if (nonwear_state > 0) {
       _Request(NWC_REQUEST_PPG); /*<< NWC_DETERM_NONWEAR. */
-      _TimerStart("活体", 0);
-    } else if (_TimerGetStatus("活体")) {
-      _Request(NWC_REQUEST_PPG); /*<< NWC_DETERM_WEAR. */
-      _TimerStart("活体", 0);
+      _TimerEnd(timer_nonwear_check_timeout);
+    } else {
+      if (timer_nonwear_check_timeout.u16DelayMs > NWC_NONWEAR_CHECK_DELAY) {
+        _Request(NWC_REQUEST_PPG); /*<< NWC_DETERM_WEAR. */
+        _TimerEnd(timer_nonwear_check_timeout);
+      }
     }
-
-    return;
   }
 
   return;
