fae3eb48a197b264f34192d59c66c9226301fbfc
Ziyi Liu <lc_lc2000@vip.163.com>
2020-12-10 17:16:04 +0800

    临时保存.

--- debug/main.c
+++ debug/main.c
@@ -103,32 +103,32 @@ int main(int argc, char **argv) {
   s.sample_length = 12;
 
 #if ALGO_DEBUG == 1
-  // char path[1000];
+  char path[1000];
 
-  // uint16_t p_count =
-  //     ListFilesRecursively(argv[1], ".csv", target_dirs, target_filenames);
+  uint16_t p_count =
+      ListFilesRecursively(argv[1], ".csv", target_dirs, target_filenames);
 
-  // int16_t accx[1000000];
-  // int16_t accy[1000000];
-  // int16_t accz[1000000];
-  // while (i < p_count) {
-  //   snprintf(path, 1000, "%s/%s", target_dirs[i], target_filenames[i]);
-  //   DebugInit(&PLOT);
+  int16_t accx[1000000];
+  int16_t accy[1000000];
+  int16_t accz[1000000];
+  while (i < p_count) {
+    snprintf(path, 1000, "%s/%s", target_dirs[i], target_filenames[i]);
+    DebugInit(&PLOT);
 
-  //   uint16_t acc_length = ReadMTKData(path, 2, accx, accy, accz, 1000000);
+    uint16_t acc_length = ReadMTKData(path, 2, accx, accy, accz, 1000000);
 
-  //   NonWalkCheck(NULL, NULL, NULL, 0, 0, 1);
-  //   for (j = 0u; j < acc_length; j += 25) {
-  //     NonWalkCheck(&accx[j], &accy[j], &accz[j], 25, 0, 0);
-  //     uint8_t result = NonWalkCheck(NULL, NULL, NULL, 25, 1, 0);
-  //     PLOT.results[PLOT.results_length++] = result;
-  //   }
+    NonWalkCheck(NULL, NULL, NULL, 0, 0, 1);
+    for (j = 0u; j < acc_length; j += 25) {
+      NonWalkCheck(&accx[j], &accy[j], &accz[j], 25, 0, 0);
+      uint8_t result = NonWalkCheck(NULL, NULL, NULL, 25, 1, 0);
+      PLOT.results[PLOT.results_length++] = result;
+    }
 
-  //   snprintf(path, 1000, "%s/%s", argv[2], target_filenames[i]);
-  //   WriteInt(path, PLOT.results, PLOT.results_length);
+    snprintf(path, 1000, "%s/%s", argv[2], target_filenames[i]);
+    WriteInt(path, PLOT.results, PLOT.results_length);
 
-  //   i++;
-  // }
+    i++;
+  }
 #else
 #include "test_data.c"
 
