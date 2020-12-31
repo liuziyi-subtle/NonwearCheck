3d56366ee9f2dd5780875850c4cc4150043e5461
root <lc_lc2000@163.com>
2020-12-31 13:41:02 +0800

    临时保存.

--- extract_features.py
+++ extract_features.py
@@ -14,24 +14,24 @@ def scale(data):
     return data
 
 
-def tsfresh_featurs(df_objects, cols2extract):
+def tsfresh_feats(df_objects, cols2extract):
     '''extract tsfresh features
     '''
     extraction_settings = ComprehensiveFCParameters()
     ids = df_objects['id'].unique()
-    df_features = []
+    df_feats = []
     for c in cols2extract:
         df = df_objects.loc[:, ['id', c]].copy()
         X = extract_features(df, column_id='id', impute_function=impute,
                              default_fc_parameters=extraction_settings)
-        df_features.append(X)
-    df_features = pd.concat(df_features, axis=1)
-    df_features['wear_category_id'] = list(
+        df_feats.append(X)
+    df_feats = pd.concat(df_feats, axis=1)
+    df_feats['wear_category_id'] = list(
         map(lambda x: df_objects[df_objects['id'] == x]['wear_category_id'].values[0], ids))
-    df_features['segment_id'] = list(
+    df_feats['segment_id'] = list(
         map(lambda x: df_objects[df_objects['id'] == x]['segment_id'].values[0], ids))
-    df_features['object_id'] = ids
-    return df_features
+    df_feats['object_id'] = ids
+    return df_feats
 
 
 def make_pairs(elements):
@@ -104,33 +104,8 @@ def ar_feature(df_objects):
 if __name__ == '__main__':
     '''
     usage:
-    python3 ./extract_features.py --object_path /data/workspace/data/objects.csv \
-                                  --feature_path /data/workspace/data/features.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/objects_hp_0.4hz.csv \
-                                  --feature_path /data/workspace/data/features_hp_0.4hz.csv
-    python3 ./extract_features.py --object_path /Users/liuziyi/Workspace/data/objects.csv \
-                                  --feature_path /Users/liuziyi/Workspace/data/features.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/objects_ir_hp_0.4hz.csv \
-                                  --feature_path /data/workspace/data/features_ir_hp_0.4hz.csv  
-    python3 ./extract_features.py --object_path /data/workspace/data/objects_ir-resample_25hz-hp_0.4hz.csv \
-                                  --feature_path /data/workspace/data/features_ir-resample_25hz-hp_0.4hz.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/objects_ir-resample_25hz.csv \
-                                  --feature_path /data/workspace/data/features_ir-resample_25hz.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/objects_ir-resample_25hz_log2.csv \
-                                  --feature_path /data/workspace/data/features_ir-resample_25hz_log2.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/objects_ir-resample_25hz_scale.csv \
-                                  --feature_path /data/workspace/data/features_ir-resample_25hz_scale.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/nonwear-check/O/results/objects_ppg-g_scale.csv \
-                                  --feature_path /data/workspace/data/nonwear-check/O/results/features_ppg-g_scale.csv
-    python3 ./extract_features.py --object_path /data/workspace/data/nonwear-check/O/results/objects_ppg-g_scale.csv \
-                                  --feature_path /data/workspace/data/nonwear-check/O/results/features_ppg-g_scale.csv
-<<<<<<< HEAD
-    python3 ./extract_features.py --object_path /data/data/nonwear-check/O/results/objects__ppg-g__object_length_24__cut_500.csv \
-                                  --feature_path /data/data/nonwear-check/O/results/features__ppg-g__object_length_24__cut_500.csv
-=======
-    python3 ./extract_features.py --object_path /data-temp/data/nonwear-check/O/results/objects__ppg-g__object_length_36__cut_500.csv \
-                                  --feature_path /data-temp/data/nonwear-check/O/results/features__ppg-g__object_length_36__cut_500.csv
->>>>>>> ed2ff8c3ada47cc26d09150e35210f331d246c89
+    python3 ./extract_features.py --object_path /data/data/NonwearCheck/450/Results/df_object_ppg_ir.csv \
+                                  --feature_path /data/data/NonwearCheck/450/Results/df_feat_ppg_ir.csv
     '''
     import argparse
     parser = argparse.ArgumentParser()
@@ -140,27 +115,25 @@ if __name__ == '__main__':
 
     df_objects = pd.read_csv(args.object_path)
 
-    df_objects['ppg'] = scale(df_objects['ppg'])
-    df_objects['ppg'] = df_objects['ppg'].astype(np.float32)
-   # df_objects = df_objects[df_objects['id'] < 50]
+    # df_objects['ppg'] = scale(df_objects['ppg'])
+    # df_objects['ppg'] = df_objects['ppg'].astype(np.float32)
+    # df_objects = df_objects[df_objects['id'] < 50]
     object_columns = list(df_objects.columns)
 
     # cols2tsfresh = set(object_columns) ^ set(
     #     ['timestamp', 'wear_category_id', 'segment_id', 'id'])
-    df_tsfresh_featurs = tsfresh_featurs(df_objects, ['ppg'])  # tsfresh特征
+    df_tsfresh_feats = tsfresh_feats(df_objects, ['ppg_ir'])  # tsfresh特征
 
     # cols2angle = ['Accelerometer X', 'Accelerometer Y', 'Accelerometer Z']
     # df_angle_features = angle_feature(df_objects, cols2angle)
 
-    df_iqrs = iqrs_feature(df_objects, ['ppg'])
+    # df_iqrs = iqrs_feature(df_objects, ['ppg'])
 
     # df_ar = ar_feature(df_objects)
-<<<<<<< HEAD
-=======
-    df_successive = successive_func(df_objects)
->>>>>>> ed2ff8c3ada47cc26d09150e35210f331d246c89
+    # df_successive = successive_func(df_objects)
 
-    # df_features = pd.concat([df_tsfresh_featurs, df_iqrs, df_ar], axis=1)
-    df_features = pd.concat([df_tsfresh_featurs, df_iqrs], axis=1)
-    df_features.to_csv(args.feature_path)
+    # df_feats = pd.concat([df_tsfresh_feats, df_iqrs, df_ar], axis=1)
+    # df_feats = pd.concat([df_tsfresh_feats, df_iqrs], axis=1)
+    df_feats = df_tsfresh_feats
+    df_feats.to_csv(args.feature_path)
     print('Done.')
